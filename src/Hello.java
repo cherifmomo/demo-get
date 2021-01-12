54e097b34aca92ebb923e9f3c71eb11fe7e13217
cherifmomo <cherifmohammed@gmail.com>
2021-01-12 19:36:47 +0100

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+package be.my;
+public class Hello {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome");
+        System.out.println("To");
+        System.out.println("Cherif");
+        System.out.println("Mohammed");
+    }
+}
